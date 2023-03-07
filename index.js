5007de161add74ee2ec6bd4320da4459def83f1a
RJ060501 <rjrussell605@gmail.com>
2023-03-06 18:59:11 -0700

    Implimented 'for each' to cycle through 3 articles rather than one. Added base property to each article so api doesn't break. Starting to implement newspaperID

--- index.js
+++ index.js
@@ -5,17 +5,55 @@ const cheerio = require('cheerio')
 
 const app = express()
 
+const newspapers = [
+    {
+        name: 'thetimes',
+        address: 'https://www.thetimes.co.uk/environment/climate-change',
+        base: ''
+    },
+    {
+        name: 'guardian',
+        address: 'https://www.theguardian.com/environment/climate-crisis',
+        base: ''
+    },
+    {
+        name: 'telegraph',
+        address: 'https://www.telegraph.co.uk/climate-change',
+        base: 'https://www.telegraph.co.uk'
+    },
+]
+
+const articles = []
+
+newspapers.forEach(newspaper => {
+    axios.get(newspaper.address)
+        .then(response => {
+            const html = response.data
+            const $ = cheerio.load(html)
+
+            $('a:contains("climate")', html).each(function () {
+                const title = $(this).text()
+                const url = $(this).attr('href')
+
+                articles.push({
+                    title,
+                    url: newspaper.base + url,
+                    source: newspaper.name
+                })
+            })
+        })
+})
+
 app.get('/', (req, res) => {
     res.json('Welcome to my Climate Change News API')
 })
 
 app.get('/news', (req, res) => {
-    axios.get('https://www.theguardian.com/environment/climate-crisis')
-        .then((response) => {
-            const html = response.data
-            console.log(html)
-        })
+    res.json(articles)
 })
 
+app.get('/news/:newspaperID', async (req, res) => {
+    console.log(req)
+})
 
 app.listen(PORT, () => console.log(`server running on PORT ${PORT}`))
